cd7c02e9cd04d357606a29033193dfc6a8c65ac6
Phan Nguyễn Anh Kiệt <anhkietphan2402.tg@gmail.com>
2025-07-10 22:36:55 +0700

    update index.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-
+import "./Navbar.css";
 function Navbar() {
   const [isLoggedIn, setLoggedIn] = useState(false);
   return (
