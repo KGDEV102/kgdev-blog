3ac8bd8f87e98fe523ea500a25bea0af1e3860ce
Phan Nguyễn Anh Kiệt <anhkietphan2402.tg@gmail.com>
2025-07-11 19:12:56 +0700

    finished navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,45 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
+import { FaBars, FaTimes } from 'react-icons/fa';
+
 function Navbar() {
   const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => setIsOpen(!isOpen);
+  const closeMenu = () => setIsOpen(false);
   return (
     <>
-      <div className="container">
-      <nav className="Navbar">
+  
+      <nav className="Navbar container">
         <NavLink to="/"><h1>KGDEV</h1></NavLink>
-        <ul className="Navbar__menu">
-          <li><NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>Home</NavLink></li>
-          <li><NavLink to="/myposts" className={({ isActive }) => isActive ? "active" : ""}>My Posts</NavLink></li>
-          <li><NavLink to="/create">Create Posts</NavLink></li>
+        <ul className={`Navbar__menu ${isOpen ? "open" : ""} `}>
+            
+             
+          <li><NavLink to="/" className={({ isActive }) => isActive ? "active" : ""} onClick={closeMenu}>Home</NavLink></li>
+          <li><NavLink to="/myposts" className={({ isActive }) => isActive ? "active" : ""} onClick={closeMenu}>My Posts</NavLink></li>
+          <li><NavLink to="/create" className={({ isActive }) => isActive ? "active" : ""} onClick={closeMenu}>Create Posts</NavLink></li>
           {
             isLoggedIn ? (
-              <li><NavLink to="/infor" className={({ isActive }) => isActive ? "active" : ""}>icon</NavLink></li>
+              <li><NavLink to="/infor" className={({ isActive }) => isActive ? "active" : ""} onClick={closeMenu}>icon</NavLink></li>
             ): (
                 <li>
-                  <span><NavLink to="/login" className={({ isActive }) => isActive ? "active" : ""}>Login</NavLink></span>
+                  <span><NavLink to="/login" className={({ isActive }) => isActive ? "active" : ""} onClick={closeMenu}>Login</NavLink></span>
                   /
-                  <span><NavLink to="/signup" className={({ isActive }) => isActive ? "active" : ""}>Signup</NavLink></span>
+                  <span><NavLink to="/signup" className={({ isActive }) => isActive ? "active" : ""} onClick={closeMenu}>Signup</NavLink></span>
               </li>   
             )
           }
+         
+          
           
         </ul>
+        <div className="Navbar__burger" onClick={toggleMenu}>
+        {isOpen ? <FaTimes /> : <FaBars />}
+        </div>
+       
       </nav>
-      </div>
+     
     </>
   )
    
